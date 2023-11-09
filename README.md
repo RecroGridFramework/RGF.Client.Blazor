607eed4913c1c84a90e324b251a8b26ffa715d82
Tamás Mikola <mikola.tamas@recrovit.hu>
2023-11-09 15:03:48 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# RecroGrid Framework Client Blazor
+
+- [RecroGrid Framework](https://RecroGrid.com)
---
+++ src/Parameters/RgfDialogParameters.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Recrovit.RecroGridFramework.Client.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recrovit.RecroGridFramework.Client.Blazor.Parameters;
+
+public class RgfDialogParameters
+{
+    public string? Title { get; set; }
+
+    public bool IsModal { get; set; } = true;
+
+    public DialogType DialogType { get; set; } = DialogType.Default;
+
+    public bool? ShowCloseButton { get; set; }
+
+    public bool? Resizable { get; set; }
+
+    public string? Width { get; set; }
+
+    public string? Height { get; set; }
+
+    public string? CssClass { get; set; }
+
+    public RenderFragment? HeaderTemplate { get; set; }
+
+    public RenderFragment? ContentTemplate { get; set; }
+
+    public RenderFragment? FooterTemplate { get; set; }
+
+    public Func<bool>? OnClose { get; set; }
+
+    public Action? Destroy { get; set; }
+
+    public IEnumerable<ButtonParameters>? PredefinedButtons { get; set; }
+
+    public bool NoHeader { get; set; } = default!;
+
+    public RenderFragment Header
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(Title))
+            {
+                return builder => builder.AddContent(0, Title);
+            }
+            if (HeaderTemplate != null)
+            {
+                return HeaderTemplate;
+            }
+            return null!;
+        }
+    }
+
+    public RenderFragment Content => ContentTemplate!;
+}
+
+public class ButtonParameters
+{
+    public ButtonParameters() { Callback = (arg) => Task.CompletedTask; }
+    public ButtonParameters(string? childText, Action<MouseEventArgs>? callback, bool isPrimary = false) : this(childText, (arg) => { callback?.Invoke(arg); return Task.CompletedTask; }, isPrimary) { }
+    public ButtonParameters(string? childText, Func<MouseEventArgs, Task>? callbackAsync = null, bool isPrimary = false)
+    {
+        ChildText = childText;
+        Callback = callbackAsync ?? ((arg) => Task.CompletedTask);
+        IsPrimary = isPrimary;
+    }
+
+    public string? ChildText { get; set; }
+
+    public bool IsPrimary { get; set; } = false;
+
+    public RenderFragment? ChildContent { get; set; }
+
+    public Func<MouseEventArgs, Task> Callback { get; set; }
+}
